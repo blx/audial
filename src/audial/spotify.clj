5b3fd4183610bfc07e4467f99ad711478f6c24f8
Ben Cook <b@bencook.ca>
2015-12-14 15:48:07 -0800

    Fix spotify search-seq function

--- src/audial/spotify.clj
+++ src/audial/spotify.clj
@@ -3,11 +3,10 @@
             [clj-http.client :as client]
             [clj-spotify.core :as sp]))
 
-;; This function is derived from clj-spotify/test/clj_spotify/core_test.clj
-
 (defn get-oauth-token
   "Returns an access token valid for Spotify Web API endpoints that
   do not require user authorization."
+  ; This function is derived from clj-spotify/test/clj_spotify/core_test.clj
   []
   (-> "https://accounts.spotify.com/api/token" 
       (client/post {:form-params {:grant_type "client_credentials"}
@@ -16,24 +15,32 @@
       :body
       :access_token))
 
-(defn search-tracks [token q & [opts]]
-  (-> (sp/search (merge {:q q
-                         :type "track"}
-                        opts)
-                 token)
-      echo
-      :tracks))
-
 (defn echo [s]
   (println s)
   s)
 
-(defn search-seq [token q]
-  (let [offset-search #(->> {:offset %}
-                            (search-tracks token q)
-                            echo)
+(defn search-tracks [token q & [opts]]
+  (println (format "searching offset=%d" (or (:offset opts) 0)))
+  (-> (sp/search (merge {:q q
+                         :type "track"}
+                        opts)
+                 token)
+      :tracks))
+
+(defn search-seq
+  "Returns a lazy-seq of search results for query q. Be aware that this
+  will transparently call Spotify's API as many times as necessary to populate
+  the seq, so, eg. (take 500 (search-seq ...)) will take some time and make
+  a dozen-ish GET requests."
+  [token q]
+  (let [chunk-size 40  ; Spotify supports [1..50] inclusive
+        offset-search #(search-tracks token q {:offset %
+                                               :limit chunk-size})
         wrap (fn wrap [{:keys [items offset limit]}]
-               (lazy-seq
-                 (when-not (empty? items)
-                   (concat items (wrap (offset-search (+ offset limit)))))))]
-    (wrap (offset-search 0))))
+               (when (seq items)
+                 (concat items (-> (+ offset limit)
+                                   offset-search
+                                   wrap
+                                   lazy-seq))))]
+    (lazy-seq
+      (wrap (offset-search 0)))))
